f9f16cba05974a0048d8d44473b5b972e4e05263
Kevin Finelli <k.finelli@gmail.com>
2013-03-17 18:49:24 -0400

    sounds, move to resources folder

---
+++ sounds.cpp
@@ -0,0 +1,14 @@
+//stl includes
+#include <string>
+#include <iostream>
+
+//sdl includes
+#include "SDL/SDL.h"
+#include "SDL/SDL_mixer.h"
+
+
+int main(int argc, char* args[]) {
+
+
+  return 0;    
+}
